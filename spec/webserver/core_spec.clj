8226d9466e75384dab395e96701c40e7e59fe844
Angeleah Daidone <angeleah@angeleah.com>
2012-12-11 17:04:28 -0600

    Add ability to return 404 not found.

--- spec/webserver/core_spec.clj
+++ spec/webserver/core_spec.clj
@@ -5,6 +5,5 @@
 (describe "it should be able to create a map of the initial request line to keys describing the parts of the line."
 
 	(it "should be able to return me a method, the request URI, and the protocol version"
-		(should= {:method "GET" :request-uri "/" :protocol-version "HTTP/1.1"}
-		(with-in-str "GET / HTTP/1.1"(process-initial-request-line))))
+		(should= {:method "GET" :request-uri "/" :protocol-version "HTTP/1.1"} (process-initial-request-line "GET / HTTP/1.1")))
 )
--- src/webserver/core.clj
+++ src/webserver/core.clj
@@ -5,22 +5,28 @@
 
 (def port 5000)
 
+(defn ok []
+	(str "HTTP/1.1 200 OK\n"
+	 	 "Content-Type: text/html\n"
+		 "\n"
+	 	 "<h1>Yo, This is my sweet web server!</h1>\n"))
+
+(defn not-found []
+	(str "HTTP/1.1 404 Not found\n"
+    	 "Content-Type: text/html\n"
+		 "\n"
+    	 "<h1>404 Not Found</h1>\n"))
+
+(defn process-initial-request-line [line]
+	(zipmap
+		[:method :request-uri :protocol-version]
+		(str/split line #"\s")))
+
 (defn webserver [in out]
 	(binding
 	      [ *in* (BufferedReader. (InputStreamReader. in))
 	        *out* (PrintWriter. out)]
-	      (println "HTTP/1.0 200 OK")
-	      (println "Content-Type: text/html")
-	      (println "")
-	      (loop [line (read-line)]
-	        (println (str line "<br/>"))
-	        (if-not (empty? line)
-	          (recur (read-line))))))
-
-(defn process-initial-request-line []
-	(zipmap
-		[:method :request-uri :protocol-version]
-		(str/split (read-line) #"\s")))
+		(if (= (:request-uri (process-initial-request-line (read-line))) "/") (println(ok)) (println(not-found)))))
 
 (defn -main []
 (create-server port webserver))
