99fed230ded150072dbf278abf2054bc4a67d18c
Angeleah Daidone <angeleah@gmail.com>
2014-01-10 20:54:18 -0600

    Tidy up.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject webserver "1.0.0-SNAPSHOT"
   :description "webserver in clojure"
-  :dependencies [[speclj "2.5.0"]
-				[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
 				[server-socket "1.0.0"]]
-  :plugins [[speclj "2.5.0"]]
-  :test-paths ["spec/"]
+  :profiles {:dev {:dependencies [[speclj "2.9.1"]]}}
+  :plugins [[speclj "2.9.1"]]
+  :test-paths ["spec"]
   :main webserver.core
   :java-source-path "src/"
 )
